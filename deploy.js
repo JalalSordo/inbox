ee7e017f10f9201044e2e3f966cd3c071de47299
Jalal Sordo <jalal_sordo@hotmail.com>
2021-09-27 02:09:10 +0100

    make deploy reusable nad quick docu

--- deploy.js
+++ deploy.js
@@ -1,22 +1,24 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');//becasause its contructor function
-const { interface, bytecode } = require("./compile")
+const { compileContract } = require("./compile")
 
 
-
-const provider = new HDWalletProvider("hockey kite dog curve enlist sick glue middle slender dentist carry beach",
+const mnemonic = "hockey kite dog curve enlist sick glue middle slender dentist carry beach";
+const provider = new HDWalletProvider(mnemonic,
     "https://ropsten.infura.io/v3/a5c57bbb4114492ebb051a84486341d7")
 const web3 = new Web3(provider);
 
-const deploy = async () => {
+
+module.exports.deploy = async (contract, initVars) => {
+    const contractToDeploy = compileContract(contract);
     const fetchedAcccounts = await web3.eth.getAccounts();
     console.log(fetchedAcccounts);
 
-    const deployedInbox = await new web3.eth.Contract("JSON.parse(interface)")
-        .deploy({ data: bytecode, arguments: ['Hi Jalal from SC'] })
+    const deployedContract = await new web3.eth.Contract(JSON.parse(contractToDeploy.interface))
+        .deploy({ data: contractToDeploy.bytecode, initVars })
         .send({ gas: '1000000', gasPrice: '5000000000', from: fetchedAcccounts[0] });
-        console.log(deployedInbox.options.address)
+    console.log(deployedContract.options.address)
 
 }
-deploy();
+
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Test Compile
+node -e "console.log(require('./compile.js').compileContract('Lottery'))";
+#Test Deploy
+node -e "console.log(require('./deploy.js').deploy('Lottery',[]))";
